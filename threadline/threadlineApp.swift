0d495ae0e61ecd4ef131e3d185f1936acf4a6e0f
sheline <sim.sheline@gmail.com>
2025-01-27 12:02:41 -0500

    initial commit

---
+++ threadline/threadlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadlineApp.swift
+//  threadline
+//
+//  Created by sheline on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct threadlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ threadlineTests/threadlineTests.swift
@@ -0,0 +1,17 @@
+//
+//  threadlineTests.swift
+//  threadlineTests
+//
+//  Created by sheline on 1/27/25.
+//
+
+import Testing
+@testable import threadline
+
+struct threadlineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
