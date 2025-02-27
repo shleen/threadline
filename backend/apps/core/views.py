d1d35de6e474ca37caf74c37d18408534c5b1766
sheline <sim.sheline@gmail.com>
2025-02-27 15:13:24 -0500

    add seed_data command, override flush command to delete clothing images from r2

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,20 +1,11 @@
-import boto3
 import time
 
 from django.http import HttpResponse, HttpResponseBadRequest
 
 from .decorators import require_method
 from .functions import get_or_create_user
+from .images import IMAGE_BUCKET, r2
 from .models import Clothing
-
-IMAGE_BUCKET = "threadline-clothing"
-
-# first param here is because boto3 was built for use with AWS S3,
-# and Cloudflare R2 is compatible with many AWS S3 SDKs
-r2 = boto3.client('s3',
-  endpoint_url='https://7b0edb7ebfca88e47f0f02147ca9274e.r2.cloudflarestorage.com',
-  region_name='enam'
-)
 
 from django.views.decorators.csrf import csrf_exempt
 
