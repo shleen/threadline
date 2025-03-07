bd6ece8e59a821127694c03636c9f4c8989828e6
sheline <sim.sheline@gmail.com>
2025-03-07 13:28:59 -0800

    log outfit frontend, plus create urlstore

--- app/threadline/LogOutfitView.swift
+++ app/threadline/LogOutfitView.swift
@@ -10,23 +10,22 @@ import SwiftUI
 // TODO: Update after integrating with backend
 struct LogOutfitItem: Identifiable {
     let id: Int
-    let item: String
+    let img_filename: String
 }
 
 struct LogOutfitView: View {
+    @AppStorage("username") private var username: String = ""
+
     @Binding var isPresented: Bool
+
+    @Environment(UrlStore.self) private var urlStore
+
+    @State var items: Array<LogOutfitItem>
 
     @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: 3)
     @State private var selectedItems: Set<Int> = []
 
-    // TODO: Integrate with backend - call /closet/get
-    var items = [
-        LogOutfitItem(id: 1, item: "one"),
-        LogOutfitItem(id: 2, item: "two"),
-        LogOutfitItem(id: 3, item: "three"),
-        LogOutfitItem(id: 4, item: "four"),
-        LogOutfitItem(id: 5, item: "five")
-    ]
+    private let closetGetRoute = "/closet/get"
 
     func saveOutfit() {
         // TODO: Save outfit - post to /outfit/post
@@ -39,19 +38,54 @@ struct LogOutfitView: View {
         VStack(spacing: 4) {
             Text("What are you wearing today?")
                 .font(.system(size: 18, weight: .regular))
+                .padding(.bottom, 16)
             ScrollView {
                 LazyVGrid(columns: gridColumns) {
                     ForEach(items) { item in
-                        LogOutfitItemView(selectedItems: $selectedItems, size: 50, item: item)
+                        LogOutfitItemView(selectedItems: $selectedItems, item: item)
                     }
                 }
             }
+            .padding(.horizontal, 16)
         }
         .toolbar {
             Button(action: saveOutfit) {
                 Text("Done")
             }
             .disabled(selectedItems.isEmpty)
+        }
+        .onAppear {
+            // Call backend to populate clothing
+            guard let apiUrl = URL(string: "\(urlStore.serverUrl)\(closetGetRoute)?username=\(username)") else {
+                print("\(closetGetRoute): Bad URL")
+                return
+            }
+
+            var request = URLRequest(url: apiUrl)
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept") // expect response in JSON
+            request.httpMethod = "GET"
+
+            Task(priority: .background) {
+                let (data, response) = try await URLSession.shared.data(for: request)
+
+                if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                    print("\(closetGetRoute): HTTP STATUS: \(httpStatus.statusCode)")
+                    return
+                }
+
+                guard let clothingReceived = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                    print("\(closetGetRoute): failed JSON deserialization")
+                    return
+                }
+
+                for c in clothingReceived["items"] as! [[String: Any]] {
+                    if let id  = c["id"] as? Int, let img_filename = c["img_filename"] as? String {
+                        items.append(
+                            LogOutfitItem(id: id, img_filename: img_filename)
+                        )
+                    }
+                }
+            }
         }
     }
 }
