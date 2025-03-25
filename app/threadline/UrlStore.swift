3595b35e50a8e2513e134f69859c52fc0825aae9
Ryan Amirthan Stephen <ryanstep@umich.edu>
2025-03-25 10:50:12 -0400

    swap backend server + r2 server

--- app/threadline/UrlStore.swift
+++ app/threadline/UrlStore.swift
@@ -2,6 +2,6 @@ import SwiftUI
 
 @Observable
 class UrlStore {
-    var serverUrl: String = "http://127.0.0.1:8000"
-    var r2BucketUrl: String = "https://threadline.sheline.me/"
+    var serverUrl: String = "https://702c-144-202-49-38.ngrok-free.app"
+    var r2BucketUrl: String = "https://pub-257d86ee7c1049beb2bba77d2f51ed91.r2.dev/"
 }
